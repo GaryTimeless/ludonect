2c7a688a5457a262dec505340555294c4ee3ecd6
GaryTimeless <gary.timeless@gmx.de>
2025-07-21 21:46:50 +0200

    supabase init

---
+++ src/supabaseClient.ts
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://lxpmcttorpjbwfbxrstl.supabase.co.supabase.co'
+const supabaseKey = 'sb_publishable_FWdd6UjiZJAGQX1aH_pLjw_xtSROVfM'
+export const supabase = createClient(supabaseUrl, supabaseKey)
