6ad8d1bc6b97535b508f91b2a1ebba36c30038f7
GaryTimeless <gary.timeless@gmx.de>
2026-03-02 22:34:25 +0100

    feat: switch to root domain ludonect.de (base path '/' + CNAME)

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/ludonect/' : '/',
+  base: '/',
   plugins: [
     vue(),
     legacy()
